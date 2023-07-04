beda0c93f8274e48393f278b61f0ae46196916bc
kays <kays6061@gmail.com>
2023-07-05 03:11:44 +1000

    added canvas

---
+++ index-script.js
@@ -0,0 +1,8 @@
+
+const canvas = document.querySelector("#canvas");
+const width = canvas.width;
+const height = canvas.height;
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "rgb(255, 255, 255)";
+ctx.fillRect(0, 0, width, height);
